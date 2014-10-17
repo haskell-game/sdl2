435e84aa6f4a593047fe6c3d9c6dbaa33338d18d
Oliver Charles <ollie@ocharles.org.uk>
2014-10-17 20:37:42 +0100

    Add convertRaw to marshal from Raw events to high-level events

--- src/SDL/Events.hs
+++ src/SDL/Events.hs
@@ -3,8 +3,10 @@ module SDL.Events (Event(..), EventPayload(..)) where
 import Foreign
 import Foreign.C
 import Linear
-import Linear.Affine (Point)
+import Linear.Affine (Point(P))
 import SDL.Raw.Types hiding (Event(..), Point)
+
+import qualified SDL.Raw as Raw
 
 data Event = Event
   { eventTimestamp :: Word32
@@ -84,3 +86,29 @@ data EventPayload
   | ClipboardUpdateEvent
   | UnknownEvent {unknownEventType :: Word32}
   deriving (Eq,Show)
+
+convertRaw :: Raw.Event -> Event
+convertRaw (Raw.WindowEvent _ ts a b c d) = Event ts (WindowEvent a b c d)
+convertRaw (Raw.KeyboardEvent _ ts a b c d) = Event ts (KeyboardEvent a b c d)
+convertRaw (Raw.TextEditingEvent _ ts a b c d) = Event ts (TextEditingEvent a b c d)
+convertRaw (Raw.TextInputEvent _ ts a b) = Event ts (TextInputEvent a b)
+convertRaw (Raw.MouseMotionEvent _ ts a b c d e f g) = Event ts (MouseMotionEvent a b c (P (V2 d e)) (V2 f g))
+convertRaw (Raw.MouseButtonEvent _ ts a b c d e f g) = Event ts (MouseButtonEvent a b c d e (P (V2 f g)))
+convertRaw (Raw.MouseWheelEvent _ ts a b c d) = Event ts (MouseWheelEvent a b (P (V2 c d)))
+convertRaw (Raw.JoyAxisEvent _ ts a b c) = Event ts (JoyAxisEvent a b c)
+convertRaw (Raw.JoyBallEvent _ ts a b c d) = Event ts (JoyBallEvent a b (V2 c d))
+convertRaw (Raw.JoyHatEvent _ ts a b c) = Event ts (JoyHatEvent a b c)
+convertRaw (Raw.JoyButtonEvent _ ts a b c) = Event ts (JoyButtonEvent a b c)
+convertRaw (Raw.JoyDeviceEvent _ ts a) = Event ts (JoyDeviceEvent a)
+convertRaw (Raw.ControllerAxisEvent _ ts a b c) = Event ts (ControllerAxisEvent a b c)
+convertRaw (Raw.ControllerButtonEvent _ ts a b c) = Event ts (ControllerButtonEvent a b c)
+convertRaw (Raw.ControllerDeviceEvent _ ts a) = Event ts (ControllerDeviceEvent a)
+convertRaw (Raw.QuitEvent _ ts) = Event ts QuitEvent
+convertRaw (Raw.UserEvent _ ts a b c d) = Event ts (UserEvent a b c d)
+convertRaw (Raw.SysWMEvent _ ts a) = Event ts (SysWMEvent a)
+convertRaw (Raw.TouchFingerEvent _ ts a b c d e f g) = Event ts (TouchFingerEvent a b (P (V2 c d)) (V2 e f) g)
+convertRaw (Raw.MultiGestureEvent _ ts a b c d e f) = Event ts (MultiGestureEvent a b c (P (V2 d e)) f)
+convertRaw (Raw.DollarGestureEvent _ ts a b c d e f) = Event ts (DollarGestureEvent a b c d (P (V2 e f)))
+convertRaw (Raw.DropEvent _ ts a) = Event ts (DropEvent a)
+convertRaw (Raw.ClipboardUpdateEvent _ ts) = Event ts ClipboardUpdateEvent
+convertRaw (Raw.UnknownEvent t ts) = Event ts (UnknownEvent t)
