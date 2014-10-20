8391bb4b02132e339b08ec3b60c69aad2b192b87
Brian McKeever <keever@gmail.com>
2014-10-20 12:10:59 -0700

    implemented accelerometer as joystick hint

--- src/SDL/Hints.hs
+++ src/SDL/Hints.hs
@@ -1,17 +1,31 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
-module SDL.Hints (Hint(..), RenderScaleQuality(..), setHint) where
+module SDL.Hints (
+    AccelerometerJoystickOptions(..),
+    Hint(..), 
+    RenderScaleQuality(..), 
+    setHint
+) where
 
 import Foreign
 import Foreign.C
 import qualified SDL.Raw as Raw
 
+data AccelerometerJoystickOptions = AccelerometerNotJoystick | AccelerometerIsJoystick
 data RenderScaleQuality = ScaleNearest | ScaleLinear | ScaleBest
 
 data Hint :: * -> * where
+  HintAccelerometerAsJoystick :: Hint AccelerometerJoystickOptions
   HintRenderScaleQuality :: Hint RenderScaleQuality
 
 setHint :: Hint v -> v -> IO Bool
+setHint (HintAccelerometerAsJoystick) v =
+  withCString "SDL_ACCELEROMETER_AS_JOYSTICK" $ \hint ->
+    withCString
+      (case v of
+         AccelerometerNotJoystick -> "0"
+         AccelerometerIsJoystick -> "1")
+      (Raw.setHint hint)
 setHint (HintRenderScaleQuality) v =
   withCString "SDL_RENDER_SCALE_QUALITY" $ \hint ->
     withCString
