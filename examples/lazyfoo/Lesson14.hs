9484562397f42c10301c4446c740d88af538598d
Oliver Charles <ollie@ocharles.org.uk>
2014-10-23 15:44:06 +0100

    Lazyfoo lesson 14. Animation seems to fast - wondering if vsync is broken...

---
+++ examples/lazyfoo/Lesson14.hs
@@ -0,0 +1,98 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Lazyfoo.Lesson14 (main) where
+
+import Control.Applicative
+import Control.Monad
+import Data.Foldable
+import Data.Monoid
+import Foreign.C.Types
+import Linear
+import Linear.Affine
+import qualified SDL
+
+screenWidth, screenHeight :: CInt
+(screenWidth, screenHeight) = (640, 480)
+
+data Texture = Texture SDL.Texture (V2 CInt)
+
+loadTexture :: SDL.Renderer -> FilePath -> IO Texture
+loadTexture r filePath = do
+  surface <- SDL.loadBMP filePath
+  size <- SDL.surfaceDimensions surface
+  format <- SDL.surfaceFormat surface
+  key <- SDL.mapRGB format (V3 0 maxBound maxBound)
+  SDL.setColorKey surface (Just key)
+  t <- SDL.createTextureFromSurface r surface
+  SDL.freeSurface surface
+  return (Texture t size)
+
+renderTexture :: SDL.Renderer -> Texture -> Point V2 CInt -> Maybe (SDL.Rectangle CInt) -> IO ()
+renderTexture r (Texture t size) xy clip =
+  let dstSize = maybe size (\(SDL.Rectangle _ size') -> size') clip
+  in SDL.renderCopy r t clip (Just (SDL.Rectangle xy dstSize))
+
+main :: IO ()
+main = do
+  SDL.init [SDL.InitVideo]
+
+  hintSet <- SDL.setHint SDL.HintRenderScaleQuality SDL.ScaleLinear
+  unless hintSet $
+    putStrLn "Warning: Linear texture filtering not enabled!"
+
+  window <-
+    SDL.createWindow
+      "SDL Tutorial"
+      SDL.defaultWindow {SDL.windowSize = V2 screenWidth screenHeight}
+  SDL.showWindow window
+
+  renderer <-
+    SDL.createRenderer
+      window
+      (-1)
+      (SDL.RendererConfig
+         { SDL.rendererAccelerated = True
+         , SDL.rendererSoftware = False
+         , SDL.rendererTargetTexture = False
+         , SDL.rendererPresentVSync = True
+         })
+
+  SDL.setRenderDrawColor renderer (V4 maxBound maxBound maxBound maxBound)
+
+  spriteSheetTexture <- loadTexture renderer "examples/lazyfoo/animation.bmp"
+  let spriteSize = V2 64 205
+      clip1 = SDL.Rectangle (P (V2 0 0)) spriteSize
+      clip2 = SDL.Rectangle (P (V2 64 0)) spriteSize
+      clip3 = SDL.Rectangle (P (V2 128 0)) spriteSize
+      clip4 = SDL.Rectangle (P (V2 196 0)) spriteSize
+
+  let loop (frame:frames) = do
+        let collectEvents = do
+              e <- SDL.pollEvent
+              case e of
+                Nothing -> return []
+                Just e' -> (e' :) <$> collectEvents
+        events <- collectEvents
+
+        let (Any quit) =
+              foldMap (\case
+                         SDL.QuitEvent -> (Any True)
+                         _ -> mempty) $
+              map SDL.eventPayload events
+
+        SDL.setRenderDrawColor renderer (V4 maxBound maxBound maxBound maxBound)
+        SDL.renderClear renderer
+
+        renderTexture renderer spriteSheetTexture (P (fmap (`div` 2) (V2 screenWidth screenHeight) - fmap (`div` 2) spriteSize)) (Just frame)
+
+        SDL.renderPresent renderer
+
+        unless quit (loop frames)
+
+  loop (cycle ([clip1, clip2, clip3, clip4] >>= replicate 4))
+
+  SDL.destroyRenderer renderer
+  SDL.destroyWindow window
+  SDL.quit
