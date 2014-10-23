8486f292d83749f4767196091bdd7155cae829f6
Gabríel Arthúr Pétursson <gabriel@system.is>
2014-10-23 01:51:55 +0000

    Add SDL.Power

--- src/SDL/Power.hs
+++ src/SDL/Power.hs
@@ -1,3 +1,45 @@
--- |
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+module SDL.Power
+  ( -- * Power Status
+    getPowerInfo
+  , PowerState(..)
+  , BatteryState(..)
+  ) where
 
-module SDL.Power where
+import Control.Applicative
+import Data.Word
+import Foreign.Ptr
+import SDL.Exception
+import SDL.Internal.Numbered
+
+import qualified SDL.Raw as Raw
+
+-- | Current power supply details.
+--
+-- Throws 'SDLException' if the current power state can not be determined.
+getPowerInfo :: IO PowerState
+getPowerInfo = do
+  -- TODO: SDL_GetPowerInfo does not set an SDL error
+  fromNumber <$> throwIf (== Raw.powerStateUnknown)
+    "SDL.Power.getPowerInfo" "SDL_GetPowerInfo"
+    (Raw.getPowerInfo nullPtr nullPtr)
+
+data PowerState
+  = Battery BatteryState
+  | Mains
+  deriving (Eq, Show)
+
+data BatteryState
+  = Draining
+  | Charged
+  | Charging
+  deriving (Eq, Show)
+
+instance FromNumber PowerState Word32 where
+  fromNumber n' = case n' of
+    n | n == Raw.powerStateOnBattery -> Battery Draining
+    n | n == Raw.powerStateNoBattery -> Mains
+    n | n == Raw.powerStateCharging -> Battery Charging
+    n | n == Raw.powerStateCharged -> Battery Charged
+    _ -> error "fromNumber: not numbered"
