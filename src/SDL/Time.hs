30c4bd3d1d5bdf3394e633f3cef62f4997bd8a67
Alexander Lehmann <afwlehmann@googlemail.com>
2014-10-23 14:03:34 +0200

    addTimer and removeTimer

--- src/SDL/Time.hs
+++ src/SDL/Time.hs
@@ -1,12 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module SDL.Time
   ( -- * Time Measurement
     ticks
   , time
+
+    -- * Timer
+  , delay
+  , TimerCallback
+  , TimerID(..)
+  , addTimer
+  , removeTimer
   ) where
 
 import Data.Word
+import Foreign
+import Foreign.C
+
+import SDL.Exception
 
 import qualified SDL.Raw.Timer as Raw
+import qualified SDL.Raw.Types as Raw
+
 
 -- | Number of milliseconds since library initialization.
 ticks :: IO Word32
@@ -21,3 +36,22 @@ time = do
 
 delay :: Word32 -> IO ()
 delay = Raw.delay
+
+foreign import ccall "wrapper"
+  mkTimerCallback :: (Word32 -> Ptr () -> IO Word32) -> IO Raw.TimerCallback
+
+type TimerCallback = Word32 -> IO Word32
+
+newtype TimerID = TimerID CInt deriving (Eq, Show)
+
+addTimer :: Word32 -> TimerCallback -> IO TimerID
+addTimer timeout callback =
+  fmap TimerID $ do
+    cb <- mkTimerCallback $ wrapCb callback
+    throwIf0 "addTimer" "SDL_AddTimer" $ Raw.addTimer timeout cb nullPtr
+  where
+    wrapCb :: TimerCallback -> Word32 -> Ptr () -> IO Word32
+    wrapCb cb = \w _ -> cb w
+
+removeTimer :: TimerID -> IO Bool
+removeTimer (TimerID t) = Raw.removeTimer t
