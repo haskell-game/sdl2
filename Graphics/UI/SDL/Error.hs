27da3f2232aaf2dd37c281da7a49262094df77a9
Gabríel Arthúr Pétursson <gabriel@system.is>
2015-05-25 00:11:05 +0000

    Add throwError to Error module

--- Graphics/UI/SDL/Error.hs
+++ Graphics/UI/SDL/Error.hs
@@ -1,17 +1,35 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Graphics.UI.SDL.Error (
   -- * Error Handling
+  SDLError(..),
+  throwError,
+
+  -- * Manual Error Handling
   clearError,
   getError,
   setError
 ) where
 
+import Control.Exception
+import Control.Monad.Catch
 import Control.Monad.IO.Class
+import Data.Typeable
 import Foreign.C.String
 import Foreign.C.Types
+
+-- | Note: the 'CString' is only valid until the next SDL function call. If you
+-- need to preserve the error message, make a copy of it.
+newtype SDLError = SDLError CString
+  deriving (Eq, Show, Typeable)
+
+instance Exception SDLError
 
 foreign import ccall "SDL.h SDL_ClearError" clearErrorFFI :: IO ()
 foreign import ccall "SDL.h SDL_GetError" getErrorFFI :: IO CString
 foreign import ccall "sdlhelper.c SDLHelper_SetError" setErrorFFI :: CString -> IO CInt
+
+throwError :: (MonadThrow m, MonadIO m) => m ()
+throwError = getError >>= throwM . SDLError
 
 clearError :: MonadIO m => m ()
 clearError = liftIO clearErrorFFI
