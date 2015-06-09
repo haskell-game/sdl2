c9f631d7cf0e5685ca8fca203b79abd41834e8a4
Oliver Charles <ollie@ocharles.org.uk>
2015-06-09 18:57:57 +0100

    Document SDL.Input.Mouse

--- src/SDL/Input/Mouse.hs
+++ src/SDL/Input/Mouse.hs
@@ -14,7 +14,7 @@ module SDL.Input.Mouse
   , MouseMotion(..)
 
     -- * Mouse State
-  , getMouseState
+  , getMouseLocation
   , getMouseButtons
 
     -- * Warping the Mouse
@@ -22,18 +22,18 @@ module SDL.Input.Mouse
   , warpMouse
 
     -- * Cursor Visibility
-  , setCursorVisible
-  , getCursorVisible
+  , cursorVisible
 
     -- * Cursor Shape
   , Cursor
-  , getCursor
-  , setCursor
+  , activeCursor
   , createCursor
   , freeCursor
   , createColorCursor
   ) where
 
+import Data.Bool
+import Data.StateVar
 import           Control.Applicative
 import           Control.Monad (void)
 import           Control.Monad.IO.Class (MonadIO, liftIO)
@@ -114,21 +114,31 @@ warpMouse :: MonadIO m => WarpMouseOrigin -> Point V2 CInt -> m ()
 warpMouse (WarpInWindow (Window w)) (P (V2 x y)) = Raw.warpMouseInWindow w x y
 warpMouse WarpCurrentFocus (P (V2 x y)) = Raw.warpMouseInWindow nullPtr x y
 
--- The usage of 'void' is OK here - Raw.showCursor just returns the old state.
-setCursorVisible :: (Functor m, MonadIO m) => Bool -> m ()
-setCursorVisible True = void $ Raw.showCursor 1
-setCursorVisible False = void $ Raw.showCursor 0
+-- | Get or set whether the cursor is currently visible.
+--
+-- This 'StateVar' can be modified using '$=' and the current value retrieved with 'get'.
+--
+-- See @<https://wiki.libsdl.org/SDL_ShowCursor SDL_ShowCursor>@ and @<https://wiki.libsdl.org/SDL_HideCursor SDL_HideCursor>@ for C documentation.
+cursorVisible :: StateVar Bool
+cursorVisible = makeStateVar getCursorVisible setCursorVisible
+  where
+  -- The usage of 'void' is OK here - Raw.showCursor just returns the old state.
+  setCursorVisible :: (Functor m, MonadIO m) => Bool -> m ()
+  setCursorVisible True = void $ Raw.showCursor 1
+  setCursorVisible False = void $ Raw.showCursor 0
 
-getCursorVisible :: (Functor m, MonadIO m) => m Bool
-getCursorVisible = (== 1) <$> Raw.showCursor (-1)
+  getCursorVisible :: (Functor m, MonadIO m) => m Bool
+  getCursorVisible = (== 1) <$> Raw.showCursor (-1)
 
-getMouseState :: MonadIO m => m (Point V2 CInt)
-getMouseState = liftIO $
+-- | Retrieve the current location of the mouse, relative to the currently focused window.
+getMouseLocation :: MonadIO m => m (Point V2 CInt)
+getMouseLocation = liftIO $
   alloca $ \x ->
   alloca $ \y -> do
     _ <- Raw.getMouseState x y -- We don't deal with button states here
     P <$> (V2 <$> peek x <*> peek y)
 
+-- | Retrieve a mapping of which buttons are currently held down.
 getMouseButtons :: MonadIO m => m (MouseButton -> Bool)
 getMouseButtons = liftIO $
   convert <$> Raw.getMouseState nullPtr nullPtr
@@ -146,26 +156,51 @@ getMouseButtons = liftIO $
 newtype Cursor = Cursor { unwrapCursor :: Raw.Cursor }
     deriving (Eq, Typeable)
 
-getCursor :: MonadIO m => m Cursor
-getCursor = liftIO . fmap Cursor $
-    throwIfNull "SDL.Input.Mouse.getCursor" "SDL_getCursor"
-        Raw.getCursor
+-- | Get or set the currently active cursor. You can create new 'Cursor's with 'createCursor'.
+--
+-- This 'StateVar' can be modified using '$=' and the current value retrieved with 'get'.
+--
+-- See @<https://wiki.libsdl.org/SDL_SetCursor SDL_SetCursor>@ and @<https://wiki.libsdl.org/SDL_GetCursor SDL_GetCursor>@ for C documentation.
+activeCursor :: StateVar Cursor
+activeCursor = makeStateVar getCursor setCursor
+  where
+  getCursor :: MonadIO m => m Cursor
+  getCursor = liftIO . fmap Cursor $
+      throwIfNull "SDL.Input.Mouse.getCursor" "SDL_getCursor"
+          Raw.getCursor
 
-setCursor :: MonadIO m => Cursor -> m ()
-setCursor = Raw.setCursor . unwrapCursor
+  setCursor :: MonadIO m => Cursor -> m ()
+  setCursor = Raw.setCursor . unwrapCursor
 
-createCursor :: MonadIO m => V.Vector Word8 -> V.Vector Word8 -> Point V2 CInt -> Point V2 CInt -> m Cursor
-createCursor dta msk (P (V2 w h)) (P (V2 hx hy)) =
+-- | Create a cursor using the specified bitmap data and mask (in MSB format).
+--
+--
+createCursor :: MonadIO m
+             => V.Vector Bool -- ^ Whether this part of the cursor is black. Use 'False' for white and 'True' for black.
+             -> V.Vector Bool -- ^ Whether or not pixels are visible. Use 'True' for visible and 'False' for transparent.
+             -> V2 CInt -- ^ The width and height of the cursor.
+             -> Point V2 CInt -- ^ The X- and Y-axis location of the upper left corner of the cursor relative to the actual mouse position
+             -> m Cursor
+createCursor dta msk (V2 w h) (P (V2 hx hy)) =
     liftIO . fmap Cursor $
         throwIfNull "SDL.Input.Mouse.createCursor" "SDL_createCursor" $
-            V.unsafeWith dta $ \unsafeDta ->
-            V.unsafeWith msk $ \unsafeMsk ->
+            V.unsafeWith (V.map (bool 0 1) dta) $ \unsafeDta ->
+            V.unsafeWith (V.map (bool 0 1) msk) $ \unsafeMsk ->
                 Raw.createCursor unsafeDta unsafeMsk w h hx hy
 
+-- | Free a cursor created with 'createCursor' and 'createColorCusor'.
+--
+-- See @<https://wiki.libsdl.org/SDL_FreeCursor SDL_FreeCursor>@ for C documentation.
 freeCursor :: MonadIO m => Cursor -> m ()
 freeCursor = Raw.freeCursor . unwrapCursor
 
-createColorCursor :: MonadIO m => Surface -> Point V2 CInt -> m Cursor
+-- | Create a color cursor.
+--
+-- See @<https://wiki.libsdl.org/SDL_CreateColorCursor SDL_CreateColorCursor>@ for C documentation.
+createColorCursor :: MonadIO m
+                  => Surface
+                  -> Point V2 CInt -- ^ The location of the cursor hot spot
+                  -> m Cursor
 createColorCursor (Surface surfPtr _) (P (V2 hx hy)) =
     liftIO . fmap Cursor $
         throwIfNull "SDL.Input.Mouse.createColorCursor" "SDL_createColorCursor" $
