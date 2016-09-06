69ec3718cdb359e1713b818cee0274145ccec85a
Theodore Lief Gannon <tanuki@gmail.com>
2016-09-06 03:28:24 -0700

    Cleaner haddock description for SDL.Vect

--- src/SDL/Vect.hs
+++ src/SDL/Vect.hs
@@ -5,10 +5,8 @@
 -- By default, re-exports the "Linear" and "Linear.Affine" modules from the
 -- 'linear' package. With the @no-linear@ Cabal flag, instead exports a
 -- duplicate implementation of the 'V2', 'V3', 'V4' and 'Point' types from
--- "SDL.Internal.Vect". This implementation provides as many instances as
--- possible for those types while avoiding any additional dependencies,
--- including the transient dependency on 'lens' and Template Haskell incurred
--- by 'linear' itself.
+-- "SDL.Internal.Vect", which provides as many instances as possible for those
+-- types while avoiding any additional dependencies.
 module SDL.Vect
   ( module Vect
   -- * Point
