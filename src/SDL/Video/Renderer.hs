2780f7f544bf86ad58c5fd011276b43d8ba4be99
Matthew Leon <ml@matthewleon.com>
2017-07-21 11:26:00 -0700

    fix a name shadowing warning
    src/SDL/Video/Renderer.hs:446:12: warning: [-Wname-shadowing]
        This binding for ‘p’ shadows the existing binding
          bound at src/SDL/Video/Renderer.hs:444:26

--- src/SDL/Video/Renderer.hs
+++ src/SDL/Video/Renderer.hs
@@ -443,8 +443,8 @@ paletteNColors (Palette p) = liftIO $ Raw.paletteNColors <$> peek p
 paletteColors :: MonadIO m => Palette -> m (Maybe (SV.Vector (V4 Word8)))
 paletteColors q@(Palette p) = do
   n <- liftIO $ fromIntegral <$> paletteNColors q
-  let wrap p | p == nullPtr = Nothing
-             | otherwise    = return p
+  let wrap p' | p' == nullPtr = Nothing
+              | otherwise     = return p'
   mv <- liftIO $ wrap . castPtr . Raw.paletteColors <$> peek p
   mColor <- liftIO $ traverse newForeignPtr_ mv
   return $ flip SV.unsafeFromForeignPtr0 n <$> mColor
