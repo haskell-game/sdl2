d519f008d9038b4fa8951c6d4e01fd23d7f4d428
Matthew Leon <ml@matthewleon.com>
2017-07-25 06:40:21 +0100

    Use PatternSynonyms extension in GameController.hs
    Needed to satisfy Travis.

--- src/SDL/Input/GameController.hs
+++ src/SDL/Input/GameController.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PatternSynonyms #-}
 
 module SDL.Input.GameController
   ( ControllerButton(..)
